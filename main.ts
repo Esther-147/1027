bfc53ecfc72e5934605b6ed92610d9c526dc3181
Esther-147 <71751338+Esther-147@users.noreply.github.com>
2020-10-27 19:20:26 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.forever(function () {
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= 4; j++) {
+            led.plot(4 - j, 4 - (i - j))
+        }
+        basic.pause(100)
+    }
+    basic.clearScreen()
+    for (let i = 0; i <= 8; i++) {
+        for (let j = 0; j <= 4; j++) {
+            led.plot(j, i - j)
+        }
+        basic.pause(100)
+    }
+    basic.clearScreen()
+})
